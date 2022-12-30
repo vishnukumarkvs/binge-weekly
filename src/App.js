9355a53472a689ed08dc4fb7d58b0ed967179878
vishnukumarkvs <kvsvishnukumar@gmail.com>
2022-12-30 19:49:31 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Switch } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+      Install & Setup  React + Tailwind CSS 3 + Chakra UI
+      </h1>
+      <Switch size='lg' className="m-2 mt-3"/>
+    </div>
+
+  );
+}
+
+export default App;
